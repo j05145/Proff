cabd203120229904d37059df7b127dae383d8670
j05145 <162765050+j05145@users.noreply.github.com>
2024-06-15 14:18:37 -0400

    Update bueno.php

--- bueno.php
+++ bueno.php
@@ -1,11 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>System Command Output:</h1>
 <?php
 $file = '/etc/passwd';
 if (file_exists($file)) {
+    echo "<pre>";
     // Leer y mostrar el contenido del archivo
     $content = file_get_contents($file);
-    header('Content-Type: text/plain'); // Establecer tipo de contenido como texto plano
-    echo $content;
+    echo htmlentities($content);
+    echo "</pre>";
 } else {
     echo "El archivo $file no existe.";
 }
 ?>
+</body>
+</html>
