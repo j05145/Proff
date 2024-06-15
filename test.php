5f02795fd7568f9aa7792cbb15d3d57c224342b1
j05145 <162765050+j05145@users.noreply.github.com>
2024-06-15 14:56:29 -0400

    Update test.php

--- test.php
+++ test.php
@@ -1,17 +1,5 @@
 <?php
-$file = '/etc/passwd';
-if (file_exists($file)) {
-    // Leer y mostrar el contenido del archivo
-    $content = file_get_contents($file);
 
-    // Establecer el tipo de contenido como texto plano
-    header('Content-Type: text/plain');
+header("Location: file:///flag");
 
-    // No es necesario establecer Content-Disposition para mostrar en línea
-
-    // Imprimir el contenido del archivo
-    echo $content;
-} else {
-    echo "El archivo $file no existe.";
-}
 ?>
